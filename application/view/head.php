b35d63939df79c03fdfdcabdb749ed64beedad19
LuanComputacao <eng.luanroger@gmail.com>
2016-07-25 13:46:29 -0300

    insert pessoa ajax

--- application/view/head.php
+++ application/view/head.php
@@ -5,6 +5,9 @@
     <link rel="stylesheet" href="/assets/css/column.css ">
     <link rel="stylesheet" href="/assets/css/style.css ">
     <link rel="stylesheet" href="/assets/css/elements.css ">
+
+
+    <script async src="https://use.fontawesome.com/9efec8aeaf.js"></script>
 </head>
 <body>
 <div class="container">
