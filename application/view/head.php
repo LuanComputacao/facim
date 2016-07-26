04d647214d1d9d169a50af67027848ce3871e428
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2016-07-26 00:00:16 -0300

    libraries and more of the form

--- application/view/head.php
+++ application/view/head.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="/assets/css/style.css ">
     <link rel="stylesheet" href="/assets/css/elements.css ">
     <link rel="stylesheet" href="/assets/css/font-awesome.min.css ">
+    <link rel="stylesheet" href="/assets/vendors/jquery-ui-1.12.0/jquery-ui.min.css ">
 </head>
 <body>
 <div class="container">
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,24 +8,50 @@ formPessoa = document.forms['form-pessoa'];
 populateTable = function (response) {
     content = JSON.parse(response);
     if (content.length > 0) {
+
         content.forEach(function (pessoa, index) {
             var tableRow = document.createElement('tr');
+            tableRow.setAttribute('data-id',pessoa.id);
             var td = '<td>' +
                 '<i class=" fa fa-pencil" aria-hidden="true" data-id="'+ pessoa.id +'"></i>' +
                 '<i class="fa fa-eraser" aria-hidden="true" data-id="'+ pessoa.id +'"></i>' +
                 '</td>';
-            if (pessoa.nome)        td += '<td>' + pessoa.nome + '</td>';
-            if (pessoa.sobrenome)   td += '<td>' + pessoa.sobrenome + '</td>';
+            if (pessoa.nome) {
+                td += '<td>' + pessoa.nome + '</td>';
+                tableRow.setAttribute('data-nome',pessoa.nome);
+            }
+            if (pessoa.sobrenome) {
+                td += '<td>' + pessoa.sobrenome + '</td>';
+                tableRow.setAttribute('data-sobrenome',pessoa.sobrenome);
+            }
+            //ENDEREÇO
             td += '<td>';
-            if (pessoa.rua)         td += pessoa.rua;
-            if (pessoa.numero)      td += ', ' + pessoa.numero;
-            if (pessoa.bairro)      td += ', ' + pessoa.bairro;
-            if (pessoa.cidade)      td += ', ' + pessoa.cidade;
-            if (pessoa.uf)          td += ' - ' + pessoa.uf;
+            if (pessoa.rua) {
+                td += pessoa.rua;
+                tableRow.setAttribute('data-rua',pessoa.rua);
+            }
+            if (pessoa.numero) {
+                td += ', ' + pessoa.numero;
+                tableRow.setAttribute('data-numero',pessoa.numero);
+            }
+            if (pessoa.bairro) {
+                td += ', ' + pessoa.bairro;
+                tableRow.setAttribute('data-bairro',pessoa.bairro);
+            }
+            if (pessoa.cidade) {
+                td += ', ' + pessoa.cidade;
+                tableRow.setAttribute('data-cidade',pessoa.cidade);
+            }
+            if (pessoa.uf) {
+                td += ' - ' + pessoa.uf;
+                tableRow.setAttribute('data-uf',pessoa.uf);
+            }
             td += '</td>';
+
             tableRow.innerHTML = td;
             table.appendChild(tableRow);
         });
+        activeButtons();
     } else {
         table.innerHTML = '<td colspan="3"> Não há pessoas cadastradas</td>'
     }
@@ -36,6 +62,45 @@ clearTable = function () {
     table.innerHTML = '';
     table.appendChild(head);
 };
+
+formErro = function (campo, msg) {
+    var msg = msg || null;
+    eCampo = document.querySelector("input[name=" + campo + "]");
+    if (msg != null) {
+        title = document.createAttribute('title');
+        title.value = msg;
+        eCampo.setAttributeNode(title);
+    }
+};
+
+activeButtons = function () {
+    var aEditors = document.getElementsByClassName('fa-pencil');
+    for (i = 0; i < aEditors.length; i++) {
+        aEditors[i].onclick = editPessoa;
+    }
+};
+
+editPessoa = function () {
+    idPessoa           = this.parentNode.parentNode.getAttribute('data-id');
+    nomePessoa         = this.parentNode.parentNode.getAttribute('data-nome');
+    sobrenomePessoa    = this.parentNode.parentNode.getAttribute('data-sobrenome');
+    ruaPessoa          = this.parentNode.parentNode.getAttribute('data-rua');
+    numeroPessoa       = this.parentNode.parentNode.getAttribute('data-numero');
+    bairroPessoa       = this.parentNode.parentNode.getAttribute('data-bairro');
+    cidadePessoa       = this.parentNode.parentNode.getAttribute('data-cidade');
+    ufPessoa           = this.parentNode.parentNode.getAttribute('data-uf');
+
+    formPessoa['id'].value          = idPessoa;
+    formPessoa['nome'].value        = nomePessoa;
+    formPessoa['sobrenome'].value   = sobrenomePessoa;
+    formPessoa['rua'].value         = ruaPessoa;
+    formPessoa['numero'].value      = numeroPessoa;
+    formPessoa['bairro'].value      = bairroPessoa;
+    formPessoa['cidade'].value      = cidadePessoa;
+    formPessoa['uf'].value          = ufPessoa;
+};
+
+
 
 ajax("POST", "pessoa", populateTable);
 
@@ -81,14 +146,4 @@ document.getElementById('form-pessoa').onsubmit = function () {
     }
 
     return false;
-};
-
-function formErro (campo, msg) {
-    var msg = msg || null;
-    eCampo = document.querySelector("input[name="+campo+"]");
-    if (msg != null) {
-        title = document.createAttribute('title');
-        title.value = msg;
-        eCampo.setAttributeNode(title) ;
-    }
 };
