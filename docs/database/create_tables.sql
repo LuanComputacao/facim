7e4fdbaba376514dd92a5226a316397cf6be4eb0
LuanComputacao <eng.luanroger@gmail.com>
2016-09-09 00:29:12 -0300

    database ddl

--- docs/database/create_tables.sql
+++ docs/database/create_tables.sql
@@ -1,25 +1,19 @@
-CREATE TABLE crossknowledge.enderecos (
-  id     INT AUTO_INCREMENT,
-  rua    VARCHAR(90),
-  numero INT,
+CREATE TABLE enderecos
+(
+  id INT(11) PRIMARY KEY NOT NULL,
+  rua VARCHAR(90),
+  numero INT(11),
   bairro VARCHAR(90),
   cidade VARCHAR(90),
-  uf     VARCHAR(45),
-  PRIMARY KEY (id)
-)
-  ENGINE = INNODB;
-
-CREATE TABLE crossknowledge.pessoas (
-id           INT NOT NULL,
-nome         VARCHAR(45),
-sobrenome    VARCHAR(45),
-fk_enderecos INT,
-PRIMARY KEY (id),
-FOREIGN KEY (fk_enderecos)
-REFERENCES enderecos (id)
-ON DELETE NO ACTION
-)
-ENGINE = INNODB;
-
-
-
+  uf VARCHAR(45)
+);
+CREATE TABLE pessoas
+(
+  id INT(11) PRIMARY KEY NOT NULL,
+  nome VARCHAR(45),
+  sobrenome VARCHAR(45),
+  fk_enderecos INT(11),
+  CONSTRAINT pessoas_ibfk_1 FOREIGN KEY (fk_enderecos) REFERENCES enderecos (id)
+);
+CREATE INDEX fk_enderecos ON pessoas (fk_enderecos);
+CREATE UNIQUE INDEX pessoas_id_uindex ON pessoas (id);
